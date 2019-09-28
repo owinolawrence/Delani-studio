9790da017015026e8a54a893b579294c2bfc0928
owinolawrence <owinolawrence21@gmail.com>
2019-09-28 09:42:26 +0300

    Create a click fuction to the images

---
+++ scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $("img").click(function(){
+      $(".p1").toggle();
+    });
+    $("img").click(function(){
+    $(".p2").toggle();
+    });
+      });
+      $(document).ready(function(){
+        $("img").click(function(){
+          $(".p3").toggle();
+        });
+      });
