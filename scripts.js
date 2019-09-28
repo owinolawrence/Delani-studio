7d3a7542ed44fb3a936d0bba6deeb3b677095001
owinolawrence <owinolawrence21@gmail.com>
2019-09-28 20:49:08 +0300

    Introduce click function to alert the message

--- scripts.js
+++ scripts.js
@@ -1,13 +1,28 @@
 $(document).ready(function () {
-  $("img").click(function () {
+  $("#image1").click(function () {
     $(".p1").toggle();
   });
   $("img").click(function () {
     $(".p2").toggle();
   });
-});
-$(document).ready(function () {
   $("img").click(function () {
     $(".p3").toggle();
-  });
+  }); a
 });
+$(document).ready(function (event) {
+
+    $("submit").click(function (event) {
+    });
+  {
+    alert("We have received your Message");
+  
+  
+    alert("Thank you for reaching us");
+  }
+
+  event.preventDefault();
+
+});
+
+
+
