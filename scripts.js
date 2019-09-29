8f90e7c1c004550a9eaeeab3373e983910fc3e69
owinolawrence <owinolawrence21@gmail.com>
2019-09-29 14:05:36 +0300

    Add hover click

--- scripts.js
+++ scripts.js
@@ -8,7 +8,32 @@ $(document).ready(function () {
   $(".image3").click(function () {
     $(".para3").toggle();
   });
+  $(".image").hover(function() {
+    $("delani1").fadeTo("slow" ,0.3);
 });
+$(".image").hover(function() {
+  $("delani2").fadeTo("slow" ,0.3);
+});
+$(".image").hover(function() {
+  $("delani3").fadeTo("slow" ,0.3);
+});
+$(".image").hover(function() {
+  $("delani4").fadeTo("slow" ,0.3);
+});
+$(".image").hover(function() {
+  $("delani5").fadeTo("slow" ,0.3);
+});
+$(".image").hover(function() {
+  $("delani6").fadeTo("slow" ,0.3);
+});
+$(".image").hover(function() {
+  $("delani7").fadeTo("slow" ,0.3);
+});
+$(".image").hover(function() {
+  $("delani8").fadeTo("slow" ,0.3);
+});
+});
+
 $(document).ready(function (event) {
 
   $(".submit").click(function (event) {
@@ -18,12 +43,4 @@ $(document).ready(function (event) {
       alert("Thank you for reaching us");
     }
   });
- 
-
-
-
-});
-
-
-
-
+ });
